f0595ba37615a1ec8b9d87abc864190e863296b8
aff97 <34793449+aff97@users.noreply.github.com>
2021-06-07 11:33:21 +0700

    trying request google.com

--- codeku.py
+++ codeku.py
@@ -1,1 +1,12 @@
+
+import requests
+
 print('Hello world')
+try:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print(r.text)
+
+except Exception as e:
+    print('ada error, e')
